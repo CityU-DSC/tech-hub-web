9eba8fdbaa4e2dc9ac74c56ed41e6e51253a9eaa
ryanyen <ryan.yen@neard.com>
2020-10-12 18:08:56 +0800

    feature: add-article-function

--- src/components/Article/index.jsx
+++ src/components/Article/index.jsx
@@ -1,25 +1,27 @@
 import React from "react";
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, Col, Row } from "react-bootstrap";
 
 const Article = ({ articles }) => {
   const articlesList = articles.length ? (
     articles.map((article) => {
       return (
-        <Card style={{ width: "22rem" }}>
-          <Card.Header as="h5">{article.author}</Card.Header>
-          <Card.Img variant="top" src={article.imageUrl} />
-          <Card.Body>
-            <Card.Title>{article.title}</Card.Title>
-            <Card.Text>{article.content}</Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
+        <Col lg={4} md={6} xs={12} className={"mt-3"} key={article.id}>
+          <Card style={{ width: "22rem" }}>
+            <Card.Header as="h5">{article.author}</Card.Header>
+            <Card.Img variant="top" src={article.imageUrl} />
+            <Card.Body>
+              <Card.Title>{article.title}</Card.Title>
+              <Card.Text>{article.content}</Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </Col>
       );
     })
   ) : (
-    <h4>You have no articles left</h4>
-  )
-  return <div>{articlesList}</div>;
+      <h4>You have no articles left</h4>
+    )
+  return <Row>{articlesList}</Row>;
 };
 
 export default Article;
